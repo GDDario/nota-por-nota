ded923e29bba0b2f8ea6febb94f1690495af436a
GDDario <gd.dario.dev@gmail.com>
2024-12-27 23:01:47 -0300

    refactor(app): add the uuid and username fields to the User model

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'uuid' => fake()->unique()->uuid(),
+            'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -37,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
