e529994023ff47579d9ebbce869cc1e50951b130
GDDario <gd.dario.dev@gmail.com>
2025-01-01 08:57:02 -0300

    chore(api): add global exception handler for AuthenticationException

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -3,17 +3,26 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Src\Domain\Exceptions\AuthenticationException;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e, Request $request) {
+            if ($e instanceof AuthenticationException) {
+                return response(['message' => $e->getMessage()],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+        });
     })->create();
