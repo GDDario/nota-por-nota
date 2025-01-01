c232ec037fc8c3388150b66a33321283d5f54968
GDDario <gd.dario.dev@gmail.com>
2025-01-01 09:14:21 -0300

    test(api): fix failing logout test

--- api/tests/Feature/Authentication/LogoutTest.php
+++ api/tests/Feature/Authentication/LogoutTest.php
@@ -6,8 +6,8 @@ use App\Models\User;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseEmpty;
 use function Pest\Laravel\post;
+use function Pest\Laravel\postJson;
 
-const LOGIN_URI = '/api/login';
 const LOGOUT_URI = '/api/logout';
 
 define('LOGOUT_URI', '/api/logout');
@@ -27,19 +27,17 @@ describe('Logout', function () {
         assertDatabaseEmpty('refresh_tokens');
     });
 
-    // FIXME: for some reason the response is getting a 500 with the message:
-    // Symfony\Component\Routing\Exception\RouteNotFoundException: Route [login] not defined
-//    it('should not logout case user is not authenticated', function () {
-//        $user = User::factory()->create([
-//            'email' => 'john@doe.com'
-//        ]);
-//
-//        $response = post(LOGOUT_URI, [], ['Authorization' => "Bearer 49|2NBX1CD3zbcWPj9ekCECxBOjmZAA5cznto9VjIIQ268d1785"]);
-//
-//        $response->assertStatus(401);
-//        $response->assertJson(['message' => 'Unauthenticated.']);
-//
-//        assertDatabaseEmpty('personal_access_tokens');
-//        assertDatabaseEmpty('refresh_tokens');
-//    });
+    it('should not logout case user is not authenticated', function () {
+        User::factory()->create([
+            'email' => 'john@doe.com'
+        ]);
+
+        $response = postJson(LOGOUT_URI, [], ['Authorization' => "Bearer 49|2NBX1CD3zbcWPj9ekCECxBOjmZAA5cznto9VjIIQ268d1785"]);
+
+        $response->assertStatus(401);
+        $response->assertJson(['message' => 'Unauthenticated.']);
+
+        assertDatabaseEmpty('personal_access_tokens');
+        assertDatabaseEmpty('refresh_tokens');
+    });
 });
