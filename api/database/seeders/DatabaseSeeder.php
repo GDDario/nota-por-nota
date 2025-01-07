fa01e0a878ba6d0117931e15831e5c44e55ae6ed
GDDario <gd.dario.dev@gmail.com>
2025-01-07 18:06:42 -0300

    refactor(api): pass the user seeder process to UserSeeder.php

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class
         ]);
-
-        User::factory(9)->create();
     }
 }
---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name'  => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'name'  => 'Jhon Doe',
+            'email' => 'jhon@doe.com',
+        ]);
+
+        User::factory(9)->create();
+    }
+}
